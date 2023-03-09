147d6c0ad60ffe47e1e490d196b2c81b205858a0
Martin Saposnik <martinrobertosaposnik@gmail.com>
2023-03-08 22:18:33 -0300

    Primer Commit

---
+++ js/javascript.js
@@ -0,0 +1,34 @@
+var buttonText = document.getElementById('button-text')
+buttonText.addEventListener("click", display)
+
+function display() {
+
+    document.getElementById("mostrar").style.display = 'block';
+}
+
+var buttonText2 = document.getElementById('button-text2')
+buttonText2.addEventListener("click", display2)
+
+function display2() {
+
+    document.getElementById("mostrar2").style.display = 'block';
+}
+
+var buttonText3 = document.getElementById('button-text3')
+buttonText3.addEventListener("click", display3)
+
+function display3() {
+
+    document.getElementById("mostrar3").style.display = 'block';
+}
+
+
+function mostrar3() {
+
+    document.getElementById('diploma1').style.display = 'block';
+}
+
+function ocultar3() {
+
+    document.getElementById('diploma1').style.display = 'none';
+}
